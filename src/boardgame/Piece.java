3a8b0d46bf82f59925193e898e44b10060cbeaaa
brunomartins32 <brunomartinns@hotmail.com>
2021-01-24 12:34:43 -0300

    Starting to implement Board and Piece2

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,4 +2,17 @@ package boardgame;
 
 public class Piece {
 
+	protected Position position;
+	private Board board;
+	
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null; //Linha opcional, por padrão o java já atribui nulo quando não preenchido
+	}
+
+
+	protected Board getBoard() {
+		return board;
+	}
 }
